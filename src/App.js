265680506f877e0dd670dd7b42cde0173a99027f
Jimmy Valencia Urbano <jvalu90@gmail.com>
2022-06-29 13:36:30 -0500

    React Form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
+import React, {Component} from 'react';
 import './App.css';
-import Title from './Title.js'
+//import Title from './Title.js'
 
-function App() {
-  const names = ['Pedro', 'Juan', 'German']
-  return (
-    <div>
-      {names.map(name => 
-        <Title name = {name} />
-      )};
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Jimmy Valencia Urbano',
+      terms: false,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <input type='text' value={this.state.name} onChange={this.updateName.bind(this)}/>
+        <div>
+          <label>
+            <input type='checkbox' checked={this.state.terms} onClick={this.updateTerms.bind(this)} /> Accept terms and conditions
+          </label>
+        </div>
+        <button onClick={this.sayHi.bind(this)}>Say Hi!</button>
+      </div>
+    );
+  }
+
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  updateTerms(event) {
+    this.setState({
+      terms: event.target.checked
+    });
+  }
+
+  sayHi() {
+    if (this.state.terms) {
+      alert('Hello ' + this.state.name);
+    } else {
+      alert('Please acept terms and conditions');
+    }    
+  }
+
 }
 
 export default App;
